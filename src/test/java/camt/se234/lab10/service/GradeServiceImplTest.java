c36f503119deebbb627373a4ac097bfd2b036af8
Unknown <frong_98_@hotmail.com>
2018-04-26 21:30:34 +0700

    Add Midterm,Final Score
    For calculate avg score

--- src/test/java/camt/se234/lab10/service/GradeServiceImplTest.java
+++ src/test/java/camt/se234/lab10/service/GradeServiceImplTest.java
@@ -1,10 +1,30 @@
 package camt.se234.lab10.service;
 
+import camt.se234.lab10.entity.Grade;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import junitparams.naming.TestCaseName;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+@RunWith(JUnitParamsRunner.class)
 public class GradeServiceImplTest {
     @Test
+    @Parameters(method = "paramsForTestGetGradeParams")
+    @TestCaseName("Test getGrade Params [{index}] : input is {0}", expect \"{1}\"")
+    public void testGetGradeparams(double score,String expectedGrade){
+        GradeServiceImpl gradeService = new GradeServiceImpl();
+        assertThat(gradeService.getGrade(score), is(expectedGrade));
+    }
+    @Test
+    @Parameters(method = "paramsForTestGetGradeParams2")
+    public void testGetGradeparams2(double midtermScore,double finalScore,String expectedGrade){
+        GradeServiceImpl gradeService = new GradeServiceImpl();
+        assertThat(gradeService.getGrade(midtermScore,finalScore), is(expectedGrade));
+    }
+
     public void testGetGrade(){
         GradeServiceImpl gradeService = new GradeServiceImpl();
         assertThat(gradeService.getGrade(100),is("A"));
@@ -18,6 +38,26 @@ public class GradeServiceImplTest {
         assertThat(gradeService.getGrade(32),is("F"));
         assertThat(gradeService.getGrade(0),is("F"));
 
+    }
+
+    public Object paramsForTestGetGradeParams(){
+        return new Object[][]{
+                {100,"A"},
+                {77,"B"},
+                {60,"C"},
+                {33,"D"},
+                {32,"F"},
+        };
+    }
+
+    public Object paramsForTestGetGradeParams2(){
+        return new Object[][]{
+                {77,88,"A"},
+                {60,95,"B"},
+                {100,20,"C"},
+                {58,20,"D"},
+                {32,30,"F"},
+        };
     }
 
 }
